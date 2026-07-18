78f558ee19eb47f4f8a5c26e59a265711227d669
Sara Elshenawy <selshenawy69@gmail.com>
2026-07-18 03:24:35 +0300

    Delete Desktop/TechPulse-Egypt directory

--- Desktop/TechPulse-Egypt/data-ingestion/utils/config_loader.py
+++
@@ -1,6 +0,0 @@
-import json
-
-
-def load_companies():
-    with open("config/companies.json", "r", encoding="utf-8") as file:
-        return json.load(file)
--- Desktop/TechPulse-Egypt/frontend/techpulse-frontend/src/services/companyService.js
+++
@@ -1,9 +0,0 @@
-import api from "./api";
-
-export const getCompanies = async () => {
-
-    const res = await api.get("/companies");
-
-    return res.data;
-
-};
